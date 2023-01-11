71490b8327da305feab1d05f33e74a0914afb481
Claire <claire.huizenga@outlook.com>
2023-01-11 20:21:59 +1100

    removed unnecessary lines of code

--- src/Components/BasicButton/BasicButton.jsx
+++ src/Components/BasicButton/BasicButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Styles from './BasicButton.module.scss';
 const BasicButton = (props) => {
   return (
-    <button className={Styles.button} id={props.id}>{props.text}</button>
+    <button className={Styles.button}>{props.text}</button>
   )
 }
 
--- src/Components/ButtonBox/ButtonBox.jsx
+++ src/Components/ButtonBox/ButtonBox.jsx
@@ -1,22 +1,12 @@
-import { render } from "@testing-library/react";
 import React from "react";
 import BasicButton from "../BasicButton/BasicButton";
 import Styles from "./ButtonBox.module.scss";
 
 const ButtonBox = () => {
-  const buttonNumberArr = [];
-  //   for (let i = 1; i <= 9; i++) {
-  //     buttonNumberArr.push(i);
-  //   }
-
   function range(start, end) {
     if (start === end) return [start];
     return [start, ...range(start + 1, end)];
   }
-
-  //   const getButtonJSX = (number) => {
-  //     <BasicButton id={number} text={number} />;
-  //   };
 
   return (
     <div className={Styles.buttonBox}>
